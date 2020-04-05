b653bcf1d04ec1eee51ba53df91bc6ee067e124b
Burak ERHANDI <bytheweyy@hotmail.com>
2020-04-05 14:57:46 +0300

    OlayController ın index view dosyası oluşturuldu. RouteConfig den başlangıç sayfası ayarlandı.

--- Ajanda_Projesi/Ajanda_Projesi/App_Start/RouteConfig.cs
+++ Ajanda_Projesi/Ajanda_Projesi/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Ajanda_Projesi
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Olay", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
