a5b4f9418776f237e8f75742029900e31c33a466
Burak ERHANDI <bytheweyy@hotmail.com>
2020-04-05 14:34:03 +0300

    Olay model dosyası ve AjandaContext oluşturuldu

---
+++ Ajanda_Projesi/Ajanda_Projesi/Models/AjandaContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Ajanda_Projesi.Models
+{
+    public class AjandaContext : DbContext
+    {
+        public AjandaContext() : base("name=AjandaContext") { }
+        public virtual DbSet<Olay> Olays { get; set; }
+    }
+}
---
+++ Ajanda_Projesi/Ajanda_Projesi/Models/Olay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ajanda_Projesi.Models
+{
+    public class Olay
+    {
+        public int id { get; set; }
+
+        public string metin { get; set; }
+
+        public DateTime baslangic { get; set; }
+
+        public DateTime bitis { get; set; }
+    }
+}
